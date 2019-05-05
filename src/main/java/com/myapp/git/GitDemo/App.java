eb7565a8695ad1d04cc4e353c0b4d40ba9ccaa97
Madhusmita <infosmita89@gmail.com>
2019-05-05 23:37:40 +0530

    added if conditon to check the data

--- src/main/java/com/myapp/git/GitDemo/App.java
+++ src/main/java/com/myapp/git/GitDemo/App.java
@@ -8,6 +8,10 @@ public class App
 {
 	public void addData(String data) {
 		System.out.println("data ="+data);
+		if(data.equals("java app"))
+		{
+			System.out.println("welcome");
+		}
 	}
     public static void main( String[] args )
     {
